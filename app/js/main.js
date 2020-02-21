d3660cab8da1ebb066789065e19c8e3677f94793
Notkabene <58079770+Notkabene@users.noreply.github.com>
2020-02-21 21:54:12 +0300

    added animation logos, when click logo

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,14 @@
 $(function(){
 
   $('#fullpage').fullpage({
-    //options here
-    autoScrolling:true,
-    scrollHorizontally: true,
     licenseKey: 'C4198C2E-BF8F4FE3-B535FFC1-A0D1CBFC',
     sectionsColor: ['#000'],
     responsiveWidth: 1024,
     responsiveHeight: 865,
   });
 
+  $('.logo').click(function(){
+    $('.logos__item').toggleClass('click-animated');
+  });
 
 });
