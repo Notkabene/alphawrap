872d660a83585bc47719db88e80e617c0635eca3
Notkabene <58079770+Notkabene@users.noreply.github.com>
2020-02-22 00:03:04 +0300

    fixed bugs

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,19 @@
 $(function(){
 
+  $('.logo').click(function(){
+    $('.logos__item').toggleClass('click-animated')
+  });
+
   $('#fullpage').fullpage({
     licenseKey: 'C4198C2E-BF8F4FE3-B535FFC1-A0D1CBFC',
     sectionsColor: ['#000'],
     responsiveWidth: 1024,
-    responsiveHeight: 865,
+    responsiveHeight: 865
   });
 
-  $('.logo').click(function(){
-    $('.logos__item').toggleClass('click-animated');
-  });
+
 
 });
+
+//('body').removeClass('fp-viewing-0'),
+//$('body').toggleClass('fp-viewing-2');$
